2a198deb8de89ffc45f657a85aba11e638b73a1f
Ethan Sutherland <ESutherland95@gmail.com>
2021-10-14 23:25:23 -0500

    removed auth server status check because it was broken for some reason

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -45,46 +45,35 @@ class Minecraft(commands.Cog):
 
         print(f'{ctx.author}({ctx.author.id}) executed Whitelist command.')
 
-        status_response: dict = get_status()
-        auth_server = status_response.get('data')[3]
-        auth_server_status = auth_server['authserver.mojang.com']
-
-        if auth_server_status == 'green':
-            if await is_rcon_enabled():
-                if await helpers.role_helper.is_role_defined('sub'):
-                    player = None
-                    try:
-                        player = Player(username=username)
-                    except:
-                        await ctx.send(
-                            embed=await helpers.embed_helper.create_error_embed(
-                                f'`{username}` is not a valid Minecraft account.'
-                            )
-                        )
-                    else:
-                        await whitelist_add_user(ctx.author.id, username)
-                        await ctx.send(
-                            embed=await helpers.embed_helper.create_success_embed(
-                                f'Set whitelist for {ctx.author.mention}: `{player.username}`',
-                                self.client.guilds[0].get_member(self.client.user.id).color
-                            )
-                        )
-                else:
+        if await is_rcon_enabled():
+            if await helpers.role_helper.is_role_defined('sub'):
+                player = None
+                try:
+                    player = Player(username=username)
+                except:
                     await ctx.send(
                         embed=await helpers.embed_helper.create_error_embed(
-                            'Sub role has not been set. Please have an administrator use `!setrole sub @<role>`.'
+                            f'`{username}` is not a valid Minecraft account.'
+                        )
+                    )
+                else:
+                    await whitelist_add_user(ctx.author.id, username)
+                    await ctx.send(
+                        embed=await helpers.embed_helper.create_success_embed(
+                            f'Set whitelist for {ctx.author.mention}: `{player.username}`',
+                            self.client.guilds[0].get_member(self.client.user.id).color
                         )
                     )
             else:
                 await ctx.send(
                     embed=await helpers.embed_helper.create_error_embed(
-                        'RCON has not been set up yet, please ask an admin to set it up.'
+                        'Sub role has not been set. Please have an administrator use `!setrole sub @<role>`.'
                     )
                 )
         else:
             await ctx.send(
                 embed=await helpers.embed_helper.create_error_embed(
-                    'Mojang auth server is offline.'
+                    'RCON has not been set up yet, please ask an admin to set it up.'
                 )
             )
 
